67102f8a2e8a5bcc3bb7a7c61354beca8f14fdfb
tkssharma <tarun.softengg@gmail.com>
2019-06-17 07:25:16 +0530

    react pwa pre cahce enabled

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+if (window.navigator && navigator.serviceWorker) {
+  navigator.serviceWorker.getRegistrations()
+    .then(function (registrations) {
+      const promise = [];
+      for (let registration of registrations) {
+        promise.push(registration.unregister());
+      }
+      return Promise.all(promise);
+    }).then((data) => {
+        navigator.serviceWorker.register('service-worker.js').then(function (registration) {
+          // Registration Success
+          console.log(registration);
+          console.log('[serviceWorker]: registration successful with scope: ', registration.scope + 'for env ' + process.env.PUBLIC_URL);
+        }).catch(function (err) {
+          // Registration failed :(
+          console.log('[serviceWorker] registration failed', err);
+        });
+    })
+}
+
